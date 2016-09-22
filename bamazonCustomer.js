42bd569fab635b112bf4cbec186531d82d6e1759
rtamang101 <tmgrohin@gmail.com>
2016-09-21 22:38:59 -0400

    fixed single query method and restarting query options

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -37,77 +37,72 @@ function queryFunction(){
 				}
 				]).then(function(order){
 
-					console.log(order.ItemID +"  "+ order.quantity);
+					// console.log(order.ItemID +"  "+ order.quantity);
 					connection.query('select * from products where ItemID ='+order.ItemID, function(err, results, fields){
-							for(i=0; i<results.length; i++){
-								var total = results[i].Price * order.quantity;
-								function total(){
-									var availableTotal = results[i].Price * results[i].StockQuantity;
-									console.log('Your total for '+results[i].ProductName+' is $'+availableTotal);
-									console.log('Thank you for shopping at Bamazon');
-								};
+
+						var item = results[0];
+						var total = item.Price * order.quantity;
+						function totalIt(){
+							console.log('total');
+							// console.log('price::', typeof results[i].Price);
+							console.log('available stock:: ', item.StockQuantity);
+							var availableTotal = item.Price * item.StockQuantity;
+							console.log('Your total for '+item.ProductName+' is $'+availableTotal);
+							console.log('Thank you for shopping at Bamazon');
+						};
 								// if (results[i].StockQuantity <1 && results[i].StockQuantity<order.quantity){
-									if(results[i].StockQuantity <1){
-										console.log('=====================================================');
-										console.log('The item you are requesting is currently out of stock');
-										console.log('  We are working on to re-stock as soon as possible');
-										console.log('        We apologize for the inconvenience');
-										console.log('=====================================================');
-										inquirer.prompt([
-												{
-													type: 'list',
-													message: 'Would you still like to continue?',
-													choices: ['Shop for other Items.', 'Done shopping'],
-													name: 'outOfInventoryList'
-												}	
-											]).then(function(outList){
-												if(outlist.outOfInventoryList == 'Shop for other Items.'){
-													queryFunction();
-												}else if(outList.outOfInventoryList == 'Done shopping'){
-													connection.end();
-												};
-											});
+						if(item.StockQuantity <1){
+							console.log('=====================================================');
+							console.log('The item you are requesting is currently out of stock');
+							console.log('  We are working on to re-stock as soon as possible');
+							console.log('        We apologize for the inconvenience');
+							console.log('=====================================================');
+							inquirer.prompt([
+								{
+									type: 'list',
+									message: 'Would you still like to continue?',
+									choices: ['Shop for other Items.', 'Done shopping'],
+									name: 'outOfInventoryList'
+								}	
+							]).then(function(outList){
+								if(outList.outOfInventoryList == 'Shop for other Items.'){
+										console.log('queryFunction type::', typeof queryFunction);
+										queryFunction();
+								}else if(outList.outOfInventoryList == 'Done shopping'){
+										connection.end();
+								};
+							});
 									
 
-									}else if(results[i].StockQuantity<order.quantity){
-										console.log('==================================================');
-										console.log('   There is only '+results[i].StockQuantity+' of the item you requested');
-										console.log('We are working on to re-stock as soon as possible');
-										console.log('       We apologize for the inconvenience');
-										console.log('==================================================');
-										inquirer.prompt([
-												{
-													type: 'list',
-													choices: ['Buy the available stock', 'Shop for other Items', 'Done shopping'],
-													message: 'Would you still like to continue',
-													name: 'lessStockList'
-												}
-											]).then(function(lessList){
-												if(lessList.lessStockList == 'Buy the available stock'){
-													// results[i].StockQuantity = order.quantity;
-													// console.log(results[i].StockQuantity);
-													// console.log(order.quantity);
-													total();
-													
-												}else if(lessList.lessStockList == 'Shop for other Items'){
-													queryFunction();
-												}else if(lessList.lessStockList == 'Done shopping'){
-													connection.end();
-												};
-											});
-									}else{
-										console.log('Your total for '+results[i].ProductName+' is $'+total);
-										console.log('Thank you for shopping at Bamazon');
+						}else if(item.StockQuantity<order.quantity){
+							console.log('==================================================');
+							console.log('   There is only '+item.StockQuantity+' of the item you requested');
+							console.log('We are working on to re-stock as soon as possible');
+							console.log('       We apologize for the inconvenience');
+							console.log('==================================================');
+							inquirer.prompt([
+									{
+										type: 'list',
+										choices: ['Buy the available stock', 'Shop for other Items', 'Done shopping'],
+										message: 'Would you still like to continue',
+										name: 'lessStockList'
+									}
+								]).then(function(lessList){
+									// console.log(lessList);
+									if(lessList.lessStockList == 'Buy the available stock'){
+										totalIt();
+									}else if(lessList.lessStockList == 'Shop for other Items'){
+										queryFunction();
+									}else if(lessList.lessStockList == 'Done shopping'){
+										connection.end();
 									};
-									// consumerQuestions();
-								
-							
-							};
-
-							
-						
-					});
+								});
+						}else{
+							console.log('Your total for '+item.ProductName+' is $'+total);
+							console.log('Thank you for shopping at Bamazon');
+						};
 				});
-	};
+			});
+		};
 	});
 };
